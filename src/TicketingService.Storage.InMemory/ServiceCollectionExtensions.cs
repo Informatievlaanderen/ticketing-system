634dafcf0ef79e0aa2aa4dd118bdc13bafcf5fd0
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-07-18 11:19:37 +0200

    fix: refine ticketing service

---
+++ src/TicketingService.Storage.InMemory/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+namespace TicketingService.Storage.InMemory;
+
+using Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddInMemoryTicketing(this IServiceCollection services, string connectionString)
+    {
+        services.AddSingleton<ITicketing, InMemoryTicketing>();
+    }
+}
--- src/TicketingService/Program.cs
+++ src/TicketingService/Program.cs
@@ -1,24 +1,68 @@
+using System.Net;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TicketingService.Abstractions;
 using TicketingService.Endpoints;
 using TicketingService.Storage.PgSqlMarten;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// add configuration
+builder.Configuration
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile("appsettings.*.json", true)
+    .AddEnvironmentVariables();
+
+// add ticketing
 builder.Services.AddMartenTicketing(builder.Configuration.GetConnectionString("Marten"));
+
+// add security
+builder.Services.AddCors();
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer();
+builder.Services.AddAuthorization(/*configure =>
+{
+  configure.FallbackPolicy = new AuthorizationPolicyBuilder()
+      .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+      .RequireAuthenticatedUser()
+      .Build();
+}*/);
+
+// add swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app
+    .UseCors()
+    .UseAuthentication()
+    .UseAuthorization();
 
 // map endpoints
-app.MapPost("/tickets/create/{originator}", Handlers.Create);
-app.MapGet("/tickets/{ticketId:guid}", Handlers.Get);
-app.MapPut("/tickets/{ticketId:guid}/pending", Handlers.Pending);
-app.MapPut("/tickets/{ticketId:guid}/complete", Handlers.Complete);
-app.MapDelete("/tickets/{ticketId:guid}", Handlers.Delete);
-app.MapGet("/tickets", Handlers.GetAll);
+app.MapPost("/tickets/create/{originator}", Handlers.Create)
+    .Produces((int)HttpStatusCode.OK)
+    .ExcludeFromDescription();
+app.MapGet("/tickets", Handlers.GetAll)
+    .Produces((int)HttpStatusCode.OK)
+    .ExcludeFromDescription();
+app.MapGet("/tickets/{ticketId:guid}", Handlers.Get)
+    .AllowAnonymous()
+    .Produces<Ticket?>();
+app.MapPut("/tickets/{ticketId:guid}/pending", Handlers.Pending)
+    .Produces((int)HttpStatusCode.OK)
+    .ExcludeFromDescription();
+app.MapPut("/tickets/{ticketId:guid}/complete", Handlers.Complete)
+    .Produces((int)HttpStatusCode.OK)
+    .ExcludeFromDescription();
+app.MapDelete("/tickets/{ticketId:guid}", Handlers.Delete)
+    .Produces((int)HttpStatusCode.OK)
+    .ExcludeFromDescription();
 
+// use swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
