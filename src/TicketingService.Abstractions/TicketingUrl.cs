9974196859178994257fdae7bc648ea00bde47f0
jvandaal <jonas.vandaal@outlook.be>
2022-09-07 14:23:26 +0200

    feat: add ticketingurl

---
+++ src/TicketingService.Abstractions/TicketingUrl.cs
@@ -0,0 +1,27 @@
+namespace TicketingService.Abstractions
+{
+    using System;
+
+    public class TicketingUrl : ITicketingUrl
+    {
+        private readonly Uri _baseUrl;
+
+        public TicketingUrl(string baseUrl) : this(new Uri(baseUrl))
+        { }
+
+        public TicketingUrl(Uri baseUrl)
+        {
+            _baseUrl = baseUrl;
+        }
+
+        public Uri For(Guid ticketId)
+        {
+            return Combine(_baseUrl, ticketId.ToString("D"));
+        }
+
+        private static Uri Combine(Uri uri, string partOfPath)
+        {
+            return new Uri($"{uri.ToString().TrimEnd('/')}/{partOfPath.TrimStart('/')}");
+        }
+    }
+}
---
+++ test/TicketingService.IntegrationTests/TicketingUrlTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TicketingService.IntegrationTests
+{
+    using System;
+    using Abstractions;
+    using FluentAssertions;
+    using Xunit;
+
+    public class TicketingUrlTests
+    {
+        [Theory]
+        [InlineData("https://api.basisregisters.vlaanderen.be/ticketing")]
+        [InlineData("https://api.basisregisters.vlaanderen.be/ticketing/")]
+        public void GivenBaseUrl_ThenIdIsAppended(string baseUrl)
+        {
+            var ticketId = Guid.NewGuid();
+
+            var sut = new TicketingUrl(baseUrl);
+
+            sut.For(ticketId).Should().Be(new Uri($"{baseUrl.TrimEnd('/')}/{ticketId:D}"));
+        }
+    }
+}
