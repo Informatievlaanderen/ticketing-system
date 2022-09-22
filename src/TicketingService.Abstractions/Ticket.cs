39b6c05de64b362ca7a85ec9eee1d19572ca0d58
Arne Dumarey <dumarey.arne@gmail.com>
2022-09-22 13:44:47 +0200

    docs: add docs to ticket

--- src/TicketingService.Abstractions/Ticket.cs
+++ src/TicketingService.Abstractions/Ticket.cs
@@ -9,9 +9,29 @@ public record Ticket(
     IDictionary<string, string> Metadata,
     TicketResult? Result = null)
 {
+    /// <summary>
+    /// De unieke identificator van het ticket.
+    /// </summary>
+    public Guid TicketId { get; set; } = TicketId;
+
+    /// <summary>
+    /// De status van het ticket.
+    /// </summary>
     public TicketStatus Status { get; set; } = Status;
+
+    /// <summary>
+    /// De resultaat van het ticket in json formaat.
+    /// </summary>
     public TicketResult? Result { get; set; } = Result;
+
+    /// <summary>
+    /// De datum waarop het ticket is aangemaakt.
+    /// </summary>
     public DateTimeOffset Created { get; init; } = DateTimeOffset.UtcNow;
+
+    /// <summary>
+    /// De datum waarop het ticket laatst is aangepast.
+    /// </summary>
     public DateTimeOffset LastModified { get; set; } = DateTimeOffset.UtcNow;
 
     public void ChangeStatus(TicketStatus newStatus, TicketResult? result = null)
