e3171892a378da4eeb05ba29732bbb0f78a2a6b2
Jan Lesage <jan.lesage@vlaanderen.be>
2023-04-18 12:24:14 +0200

    fix: TicketError equality checks

---
+++ test/TicketingService.Storage.InMemory.Tests/TicketErrorEqualityTests.cs
@@ -0,0 +1,35 @@
+namespace TicketingService.Storage.InMemory.Tests;
+
+using Abstractions;
+using Xunit;
+
+public class TicketErrorEqualityTests
+{
+    [Fact]
+    public void ItShouldBeEqualWhenSingular()
+    {
+        var ticketError1 = new TicketError("ErrorMessage", "ErrorCode");
+        var ticketError2 = new TicketError("ErrorMessage", "ErrorCode");
+
+        Assert.Equal(ticketError1, ticketError2);
+    }
+
+    [Fact]
+    public void ItShouldBeEqualWhenMultiple()
+    {
+        var ticketError1 = new TicketError(new[]
+        {
+            new TicketError("ErrorMessage1", "ErrorCode1"),
+            new TicketError("ErrorMessage2", "ErrorCode2"),
+            new TicketError("ErrorMessage3", "ErrorCode3")
+        });
+        var ticketError2 = new TicketError(new[]
+        {
+            new TicketError("ErrorMessage1", "ErrorCode1"),
+            new TicketError("ErrorMessage2", "ErrorCode2"),
+            new TicketError("ErrorMessage3", "ErrorCode3")
+        });
+
+        Assert.Equal(ticketError1, ticketError2);
+    }
+}
