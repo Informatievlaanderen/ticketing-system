f87e8962686ea54ee928500f75e7813bdffb73f5
Arne Dumarey <dumarey.arne@gmail.com>
2023-04-28 16:20:20 +0200

    feat: first try add a metric

--- src/TicketingService/Endpoints/Handlers.cs
+++ src/TicketingService/Endpoints/Handlers.cs
@@ -5,12 +5,25 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Abstractions;
+using Elastic.Apm;
 using Microsoft.AspNetCore.Mvc;
 
 public static class Handlers
 {
     public static async Task<Guid> Create([FromBody] IDictionary<string, string>? metadata, ITicketing ticketing, CancellationToken cancellationToken = default)
-        => await ticketing.CreateTicket(metadata, cancellationToken);
+    {
+        var transaction = Agent.Tracer.StartTransaction("TicketLifespan", "CreateTicket");
+        var createSpan = transaction.StartSpan("Create", "Operation");
+
+        var ticketId = await ticketing.CreateTicket(metadata, cancellationToken);
+
+        transaction.SetLabel("TicketId", ticketId.ToString());
+
+        createSpan.End();
+        transaction.End();
+
+        return ticketId;
+    }
 
     public static async Task<IEnumerable<Ticket>> GetAll(ITicketing ticketing, CancellationToken cancellationToken = default)
         => await ticketing.GetAll(cancellationToken);
@@ -22,11 +35,38 @@ public static class Handlers
         => await ticketing.Pending(ticketId, cancellationToken);
 
     public static async Task Complete(Guid ticketId, TicketResult result, ITicketing ticketing, CancellationToken cancellationToken = default)
-        => await ticketing.Complete(ticketId, result, cancellationToken);
+    {
+        var transaction = Agent.Tracer.StartTransaction("TicketLifespan", "CompleteTicket");
+        var completeSpan = transaction.StartSpan("Complete", "Operation");
+        transaction.SetLabel("TicketId", ticketId.ToString());
+
+        await ticketing.Complete(ticketId, result, cancellationToken);
+
+        completeSpan.End();
+        transaction.End();
+    }
 
     public static async Task Error(Guid ticketId, TicketError error, ITicketing ticketing, CancellationToken cancellationToken = default)
-        => await ticketing.Error(ticketId, error, cancellationToken);
+    {
+        var transaction = Agent.Tracer.StartTransaction("TicketLifespan", "CompleteTicket");
+        var errorSpan = transaction.StartSpan("Error", "Operation");
+        transaction.SetLabel("TicketId", ticketId.ToString());
+
+        await ticketing.Error(ticketId, error, cancellationToken);
+
+        errorSpan.End();
+        transaction.End();
+    }
 
     public static async Task Delete(Guid ticketId, ITicketing ticketing, CancellationToken cancellationToken = default)
-        => await ticketing.Delete(ticketId, cancellationToken);
+    {
+        var transaction = Agent.Tracer.StartTransaction("TicketLifespan", "CompleteTicket");
+        var createSpan = transaction.StartSpan("Delete", "Operation");
+        transaction.SetLabel("TicketId", ticketId.ToString());
+
+        await ticketing.Delete(ticketId, cancellationToken);
+
+        createSpan.End();
+        transaction.End();
+    }
 }
