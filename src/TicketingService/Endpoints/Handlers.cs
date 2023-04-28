1033c75799223ba8157757af0bfb0b7ef1b3cde7
Arne Dumarey <dumarey.arne@gmail.com>
2023-04-28 18:48:55 +0200

    fix: try metrics another way

--- src/TicketingService/Endpoints/Handlers.cs
+++ src/TicketingService/Endpoints/Handlers.cs
@@ -12,16 +12,14 @@ public static class Handlers
 {
     public static async Task<Guid> Create([FromBody] IDictionary<string, string>? metadata, ITicketing ticketing, CancellationToken cancellationToken = default)
     {
-        var transaction = Agent.Tracer.StartTransaction("TicketLifespan", "CreateTicket");
-        var createSpan = transaction.StartSpan("Create", "Operation");
-
-        var ticketId = await ticketing.CreateTicket(metadata, cancellationToken);
-
-        transaction.SetLabel("TicketId", ticketId.ToString());
-
-        createSpan.End();
-        transaction.End();
-
+        var ticketId = Guid.Empty;
+        await Agent.Tracer.CaptureTransaction("CreateTicket", "TicketLifespan", async transaction =>
+        {
+            ticketId = await ticketing.CreateTicket(metadata, cancellationToken);
+            transaction.SetLabel("TicketId", ticketId.ToString());
+            transaction.SetLabel("Status", "Created");
+            transaction.SetLabel("CreatedTimestamp", DateTime.UtcNow.ToString("o"));
+        });
         return ticketId;
     }
 
@@ -32,41 +30,46 @@ public static class Handlers
         => await ticketing.Get(ticketId, cancellationToken);
 
     public static async Task Pending(Guid ticketId, ITicketing ticketing, CancellationToken cancellationToken = default)
-        => await ticketing.Pending(ticketId, cancellationToken);
+    {
+        await Agent.Tracer.CaptureTransaction("PendingTicket", "TicketLifespan", async transaction =>
+        {
+            await ticketing.Pending(ticketId, cancellationToken);
+            transaction.SetLabel("TicketId", ticketId.ToString());
+            transaction.SetLabel("Status", "Pending");
+            transaction.SetLabel("LastChangedTimestamp", DateTime.UtcNow.ToString("o"));
+        });
+    }
 
     public static async Task Complete(Guid ticketId, TicketResult result, ITicketing ticketing, CancellationToken cancellationToken = default)
     {
-        var transaction = Agent.Tracer.StartTransaction("TicketLifespan", "CompleteTicket");
-        var completeSpan = transaction.StartSpan("Complete", "Operation");
-        transaction.SetLabel("TicketId", ticketId.ToString());
-
-        await ticketing.Complete(ticketId, result, cancellationToken);
-
-        completeSpan.End();
-        transaction.End();
+        await Agent.Tracer.CaptureTransaction("CompleteTicket", "TicketLifespan", async transaction =>
+        {
+            await ticketing.Complete(ticketId, result, cancellationToken);
+            transaction.SetLabel("TicketId", ticketId.ToString());
+            transaction.SetLabel("Status", "Complete");
+            transaction.SetLabel("LastChangedTimestamp", DateTime.UtcNow.ToString("o"));
+        });
     }
 
     public static async Task Error(Guid ticketId, TicketError error, ITicketing ticketing, CancellationToken cancellationToken = default)
     {
-        var transaction = Agent.Tracer.StartTransaction("TicketLifespan", "CompleteTicket");
-        var errorSpan = transaction.StartSpan("Error", "Operation");
-        transaction.SetLabel("TicketId", ticketId.ToString());
-
-        await ticketing.Error(ticketId, error, cancellationToken);
-
-        errorSpan.End();
-        transaction.End();
+        await Agent.Tracer.CaptureTransaction("ErrorTicket", "TicketLifespan", async transaction =>
+        {
+            await ticketing.Error(ticketId, error, cancellationToken);
+            transaction.SetLabel("TicketId", ticketId.ToString());
+            transaction.SetLabel("Status", "Error");
+            transaction.SetLabel("LastChangedTimestamp", DateTime.UtcNow.ToString("o"));
+        });
     }
 
     public static async Task Delete(Guid ticketId, ITicketing ticketing, CancellationToken cancellationToken = default)
     {
-        var transaction = Agent.Tracer.StartTransaction("TicketLifespan", "CompleteTicket");
-        var createSpan = transaction.StartSpan("Delete", "Operation");
-        transaction.SetLabel("TicketId", ticketId.ToString());
-
-        await ticketing.Delete(ticketId, cancellationToken);
-
-        createSpan.End();
-        transaction.End();
+        await Agent.Tracer.CaptureTransaction("DeleteTicket", "TicketLifespan", async transaction =>
+        {
+            await ticketing.Delete(ticketId, cancellationToken);
+            transaction.SetLabel("TicketId", ticketId.ToString());
+            transaction.SetLabel("Status", "Deleted");
+            transaction.SetLabel("LastChangedTimestamp", DateTime.UtcNow.ToString("o"));
+        });
     }
 }
