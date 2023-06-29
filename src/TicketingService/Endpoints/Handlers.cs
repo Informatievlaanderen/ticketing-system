bbb00376f9fc2c712fa49a92c094643e99b474ce
Arne Dumarey <dumarey.arne@gmail.com>
2023-06-29 14:11:49 +0200

    feat: add datadog spans

--- src/TicketingService/Endpoints/Handlers.cs
+++ src/TicketingService/Endpoints/Handlers.cs
@@ -5,70 +5,89 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Abstractions;
+using Datadog.Trace;
+using Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 public static class Handlers
 {
-    public static async Task<Guid> Create([FromBody] IDictionary<string, string>? metadata, ITicketing ticketing, CancellationToken cancellationToken = default)
+    public static async Task<Guid> Create([FromBody] IDictionary<string, string>? metadata, ITicketing ticketing,
+        CancellationToken cancellationToken = default)
     {
         var ticketId = Guid.Empty;
-        // await Agent.Tracer.CaptureTransaction("CreateTicket", "TicketLifespan", async transaction =>
-        // {
         ticketId = await ticketing.CreateTicket(metadata, cancellationToken);
-        //     transaction.SetLabel("TicketId", ticketId.ToString());
-        //     transaction.SetLabel("Status", "Created");
-        //     transaction.SetLabel("CreatedTimestamp", DateTime.UtcNow.ToString("o"));
-        // });
+
+        using(var scope = Tracer.Instance.StartActive("CreateTicket", new SpanCreationSettings { Parent = new SpanContext(null, ticketId.ToULong()) }))
+        {
+            scope.Span.SetTag("ticketId", ticketId.ToString("D"));
+            scope.Span.SetTag("status", "Created");
+            scope.Span.SetTag("createdTimestamp", DateTime.UtcNow.ToString("o"));
+        }
+
         return ticketId;
     }
 
-    public static async Task<IEnumerable<Ticket>> GetAll(ITicketing ticketing, CancellationToken cancellationToken = default)
+    public static async Task<IEnumerable<Ticket>> GetAll(ITicketing ticketing,
+        CancellationToken cancellationToken = default)
         => await ticketing.GetAll(cancellationToken);
 
-    public static async Task<Ticket?> Get(Guid ticketId, ITicketing ticketing, CancellationToken cancellationToken = default)
+    public static async Task<Ticket?> Get(Guid ticketId, ITicketing ticketing,
+        CancellationToken cancellationToken = default)
         => await ticketing.Get(ticketId, cancellationToken);
 
     public static async Task Pending(Guid ticketId, ITicketing ticketing, CancellationToken cancellationToken = default)
     {
-        // await Agent.Tracer.CaptureTransaction("PendingTicket", "TicketLifespan", async transaction =>
-        // {
-        await ticketing.Pending(ticketId, cancellationToken);
-        //     transaction.SetLabel("TicketId", ticketId.ToString());
-        //     transaction.SetLabel("Status", "Pending");
-        //     transaction.SetLabel("LastChangedTimestamp", DateTime.UtcNow.ToString("o"));
-        // });
+        using(var scope = Tracer.Instance.StartActive("SetTicketPending", new SpanCreationSettings { Parent = new SpanContext(null, ticketId.ToULong()) }))
+        {
+            await ticketing.Pending(ticketId, cancellationToken);
+
+            scope.Span.SetTag("ticketId", ticketId.ToString("D"));
+            scope.Span.SetTag("status", "Pending");
+            scope.Span.SetTag("lastChangedTimestamp", DateTime.UtcNow.ToString("o"));
+        }
     }
 
-    public static async Task Complete(Guid ticketId, TicketResult result, ITicketing ticketing, CancellationToken cancellationToken = default)
+    public static async Task Complete(
+        Guid ticketId,
+        TicketResult result,
+        ITicketing ticketing,
+        CancellationToken cancellationToken = default)
     {
-        // await Agent.Tracer.CaptureTransaction("CompleteTicket", "TicketLifespan", async transaction =>
-        // {
-        await ticketing.Complete(ticketId, result, cancellationToken);
-        //     transaction.SetLabel("TicketId", ticketId.ToString());
-        //     transaction.SetLabel("Status", "Complete");
-        //     transaction.SetLabel("LastChangedTimestamp", DateTime.UtcNow.ToString("o"));
-        // });
+        using(var scope = Tracer.Instance.StartActive("CompleteTicket", new SpanCreationSettings { Parent = new SpanContext(null, ticketId.ToULong()) }))
+        {
+            await ticketing.Complete(ticketId, result, cancellationToken);
+
+            scope.Span.SetTag("ticketId", ticketId.ToString("D"));
+            scope.Span.SetTag("status", "Complete");
+            scope.Span.SetTag("lastChangedTimestamp", DateTime.UtcNow.ToString("o"));
+        }
     }
 
-    public static async Task Error(Guid ticketId, TicketError error, ITicketing ticketing, CancellationToken cancellationToken = default)
+    public static async Task Error(
+        Guid ticketId,
+        TicketError error,
+        ITicketing ticketing,
+        CancellationToken cancellationToken = default)
     {
-    //     await Agent.Tracer.CaptureTransaction("ErrorTicket", "TicketLifespan", async transaction =>
-    //     {
-        await ticketing.Error(ticketId, error, cancellationToken);
-    //         transaction.SetLabel("TicketId", ticketId.ToString());
-    //         transaction.SetLabel("Status", "Error");
-    //         transaction.SetLabel("LastChangedTimestamp", DateTime.UtcNow.ToString("o"));
-    //     });
+        using(var scope = Tracer.Instance.StartActive("ErrorTicket", new SpanCreationSettings { Parent = new SpanContext(null, ticketId.ToULong()) }))
+        {
+            await ticketing.Error(ticketId, error, cancellationToken);
+
+            scope.Span.SetTag("ticketId", ticketId.ToString("D"));
+            scope.Span.SetTag("status", "Error");
+            scope.Span.SetTag("lastChangedTimestamp", DateTime.UtcNow.ToString("o"));
+        }
     }
 
     public static async Task Delete(Guid ticketId, ITicketing ticketing, CancellationToken cancellationToken = default)
     {
-        // await Agent.Tracer.CaptureTransaction("DeleteTicket", "TicketLifespan", async transaction =>
-        // {
-        await ticketing.Delete(ticketId, cancellationToken);
-        //     transaction.SetLabel("TicketId", ticketId.ToString());
-        //     transaction.SetLabel("Status", "Deleted");
-        //     transaction.SetLabel("LastChangedTimestamp", DateTime.UtcNow.ToString("o"));
-        // });
+        using(var scope = Tracer.Instance.StartActive("DeleteTicket", new SpanCreationSettings { Parent = new SpanContext(null, ticketId.ToULong()) }))
+        {
+            await ticketing.Delete(ticketId, cancellationToken);
+
+            scope.Span.SetTag("ticketId", ticketId.ToString("D"));
+            scope.Span.SetTag("status", "Deleted");
+            scope.Span.SetTag("lastChangedTimestamp", DateTime.UtcNow.ToString("o"));
+        }
     }
 }
---
+++ src/TicketingService/Extensions/GuidExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace TicketingService.Extensions;
+
+using System;
+using System.Security.Cryptography;
+
+public static class GuidExtensions
+{
+    public static ulong ToULong(this Guid guid)
+    {
+        var guidBytes = guid.ToByteArray();
+
+        // Use SHA-256 hash algorithm
+        using var sha256 = SHA256.Create();
+        var hashBytes = sha256.ComputeHash(guidBytes);
+
+        // Take the first 8 bytes (64 bits) of the hash
+        var ulongBytes = new byte[8];
+        Array.Copy(hashBytes, ulongBytes, 8);
+
+        // Convert the byte array to a ulong integer
+        var ulongValue = BitConverter.ToUInt64(ulongBytes, 0);
+
+        return ulongValue;
+    }
+}
