02afa57ed0a9ef825bef600ea26f2f3f4f817158
pgallik <patrickgallik@gmail.com>
2023-08-24 14:33:26 +0200

    fix: query open tickets

---
+++ test/TicketingService.Monitoring.Tests/SetupMartenFixture.cs
@@ -0,0 +1,39 @@
+﻿namespace TicketingService.Monitoring.Tests
+{
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.DockerUtilities;
+    using Ductus.FluentDocker.Services;
+    using Marten;
+    using Microsoft.Extensions.DependencyInjection;
+    using Storage.PgSqlMarten;
+    using Xunit;
+
+    public class SetupMartenFixture : IAsyncLifetime
+    {
+        private ICompositeService _container;
+        public IDocumentStore DocumentStore { get; private set; }
+        public MartenTicketing MartenTicketing { get; private set; }
+
+        public Task InitializeAsync()
+        {
+            _container = DockerComposer.Compose("postgres_test.yml", "ticketing-system-monitoring-integration-tests");
+            const string connectionString =
+                "Host=localhost;Port=5435;Database=tickets;Username=postgres;Password=postgres";
+
+            var services = new ServiceCollection();
+            services.AddMartenTicketing(connectionString);
+            var serviceProvider = services.BuildServiceProvider();
+
+            DocumentStore = serviceProvider.GetRequiredService<IDocumentStore>();
+            MartenTicketing = new MartenTicketing(DocumentStore);
+
+            return Task.CompletedTask;
+        }
+
+        public Task DisposeAsync()
+        {
+            _container.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
