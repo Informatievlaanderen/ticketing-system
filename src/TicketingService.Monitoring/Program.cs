9b2082eebd0afed338a82602a1252f2dfa5dfc57
emalfroy <egon.malfroy@gmail.com>
2024-03-15 15:33:23 +0100

    fix: register addhttplogging

--- src/TicketingService.Monitoring/Program.cs
+++ src/TicketingService.Monitoring/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using TicketingService.Monitoring;
 
-var app = WebApplication
-    .CreateBuilder(args)
+var appBuilder = WebApplication
+    .CreateBuilder(args);
+appBuilder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = HttpLoggingFields.All;
+    logging.RequestBodyLogLimit = 4096;
+});
+var app = appBuilder
     .AddAppSettings(args)
     .AddOptions<ConnectionStrings>()
     .AddOptions<NotificationsOptions>()
