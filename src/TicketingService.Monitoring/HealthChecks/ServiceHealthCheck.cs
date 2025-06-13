4605d392f0096c2412f2eb1627c63216ce9113fc
Arne Dumarey <dumarey.arne@gmail.com>
2025-06-13 13:53:54 +0200

    fix(monitoring): fix ctor servicehealthcheck

--- src/TicketingService.Monitoring/HealthChecks/ServiceHealthCheck.cs
+++ src/TicketingService.Monitoring/HealthChecks/ServiceHealthCheck.cs
@@ -8,12 +8,10 @@ using Microsoft.Extensions.Hosting;
 public sealed class ServiceHealthCheck<T> : IHealthCheck where T : IHostedService, IHealthCheckable
 {
     private readonly T _service;
-    private readonly string _name;
 
-    public ServiceHealthCheck(T service, string name)
+    public ServiceHealthCheck(T service)
     {
         _service = service;
-        _name = name;
     }
 
     public Task<HealthCheckResult> CheckHealthAsync(
@@ -21,7 +19,7 @@ public sealed class ServiceHealthCheck<T> : IHealthCheck where T : IHostedServic
         CancellationToken cancellationToken = default)
     {
         return Task.FromResult(_service.IsHealthy
-            ? HealthCheckResult.Healthy($"{_name} is running normally")
-            : HealthCheckResult.Unhealthy($"{_name} is not running or has failed"));
+            ? HealthCheckResult.Healthy($"{_service.GetType().Name} is running normally")
+            : HealthCheckResult.Unhealthy($"{_service.GetType().Name} is not healthy"));
     }
 }
